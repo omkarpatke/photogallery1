1cb65ae155a79ba136b27da328ed5656bf3057ed
OMKAR SANJAY PATKE <76443142+omkarpatke@users.noreply.github.com>
2021-04-24 13:40:00 +0530

    update: style

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,9 +3,11 @@ import React from "react";
 const Title = () => {
   return (
     <div className="title">
-      <h1>Gallery</h1>
-      <h2>Your Pictures</h2>
-      <p>Add your photos and Save your memories.</p>
+      <h1 style={{ fontSize: "2rem" }}>Gallery</h1>
+      <h2 style={{ fontSize: "3rem", background: "#efb6b2" }}>Your Pictures</h2>
+      <p style={{ fontSize: "1.5rem" }}>
+        Add your photos and Save your memories.
+      </p>
     </div>
   );
 };
